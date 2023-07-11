7ef071ded7031b354a0ce06458250be33ebb71a3
Marc Esquerra <esquerra@gmail.com>
2023-07-11 18:23:38 +0100

    Update to new nickel dependency. Add sample test

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
 use config_finder::ConfigDirs;
-use nickel_lang::eval::cache::CacheImpl;
-use nickel_lang::program::Program;
-use nickel_lang::term::RichTerm;
+use nickel_lang_core::eval::cache::CacheImpl;
+use nickel_lang_core::program::Program;
+use nickel_lang_core::term::RichTerm;
 use serde::Deserialize;
 use std::path::PathBuf;
 
@@ -21,8 +21,8 @@ pub type Result<T> = std::result::Result<T, Error>;
 #[derive(Debug, Clone, PartialEq)]
 pub enum Error {
     ConfigFileReadingError(String),
-    NickelEvaluationError(nickel_lang::error::Error),
-    RustDeserializationError(nickel_lang::deserialize::RustDeserializationError),
+    NickelEvaluationError(nickel_lang_core::error::Error),
+    RustDeserializationError(nickel_lang_core::deserialize::RustDeserializationError),
 }
 
 fn expand_names(mut pb1: PathBuf) -> Vec<PathBuf> {
@@ -59,12 +59,27 @@ fn first_existing_config(app: &str) -> Option<PathBuf> {
 }
 
 fn load<'a, T: Deserialize<'a>>(path: PathBuf) -> Result<T> {
-    let mut program: Program<CacheImpl> =
-        Program::new_from_file(path).map_err(|e| Error::ConfigFileReadingError(e.to_string()))?;
+    let mut program: Program<CacheImpl> = Program::new_from_file(path, std::io::stderr())
+        .map_err(|e| Error::ConfigFileReadingError(e.to_string()))?;
     let rt: RichTerm = program
         .eval_full_for_export()
         .map(RichTerm::from)
         .map_err(Error::NickelEvaluationError)?;
 
     T::deserialize(rt).map_err(Error::RustDeserializationError)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn ensure_expand_names_work() {
+        let result = expand_names(PathBuf::from("/tmp"));
+        let expected: Vec<PathBuf> = vec![
+            PathBuf::from("/tmp/config.ncl"),
+            PathBuf::from("/tmp/config.nickel"),
+        ];
+        assert_eq!(result, expected);
+    }
 }
